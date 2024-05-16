a3fae2e945cc8e5a8f2d02a224462cdb60a2b2a2
NatnaelWale <nhattyjeje@gmail.com>
2024-05-16 10:48:57 -0400

    Worked on filter and find methods of JS

--- ES6Scripts/es6_course_search.js
+++ ES6Scripts/es6_course_search.js
@@ -38,5 +38,70 @@ let courses = [
   },
 ];
 
+// When does the PROG200 course start?
+
+// ========1st way by writing a whole function======//
+
+// function getProgramStartDate(course){
+//    return course.CourseId === "PROG200";
+// }
+// console.log(courses.find(getProgramStartDate).StartDate);
+
+// ========2nd way by writing an arrow function======//
+
+// const outPut1 = courses.find((course) => course.CourseId === "PROG200");
+// console.log(outPut1.StartDate)
 
 
+
+// What is the title of the PROJ500 course?
+
+// ========1st way by writing a whole function======//
+
+// function getTitleByCourseId(course){
+//   return course.CourseId === "PROJ500";
+// };
+
+// console.log(courses.find(getTitleByCourseId).Title)
+
+// ========2nd way by writing an arrow function======//
+
+// let outPut2 = courses.find((course) => course.CourseId === "PROJ500");
+
+// console.log(outPut2.Title);
+
+
+
+// What are the titles of the courses that cost $50 or less?
+
+// ========1st way by writing a whole function======//
+
+// function getCoursesWithPrice(course) {
+//     return course.Fee <= 50;
+//   }
+
+
+// console.log(courses.filter(getCoursesWithPrice));
+
+
+// ========2nd way by writing an arrow function======//
+
+// const outPut3 = courses.filter(course => course.Fee <= 50).map(course => course.Title);
+
+// console.log(outPut3);
+
+
+
+// What classes meet in "Classroom 1"?
+
+// ========1st way by writing a whole function======//
+// function getClassesByClassroom(course) {
+//   return course.Location === "Classroom 1";
+// }
+
+// console.log(courses.filter(getClassesByClassroom))
+
+// ========2nd way by writing an arrow function======//
+// let classRoom1Classes = courses.filter((course) => course.Location === "Classroom 1").map((course) => course.Title);
+
+// console.log(classRoom1Classes);
